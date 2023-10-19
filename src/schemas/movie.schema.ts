d7b97a29b9abce342c370a6a3569f98978ab5296
bervalim <bernardogvalim@gmail.com>
2023-10-19 16:37:35 -0300

    feat/zodValidation

---
+++ src/schemas/movie.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const movieResponseSchema = z.object({
+  id: z.number().positive(),
+  name: z.string().max(50).min(3),
+  description: z.string().nullish(),
+  duration: z.number().positive(),
+  price: z.number().positive(),
+});
+
+export const movieRequestSchema = movieResponseSchema.omit({ id: true });
+export const movieUpdateSchema = movieRequestSchema.partial();
